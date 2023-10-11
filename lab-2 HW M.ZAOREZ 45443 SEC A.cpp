96a137b98bfd9fbd8d851033e8c2605a7251343a
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-10-10 23:51:07 -0700

    Add files via upload

---
+++ lab-2 HW M.ZAOREZ 45443 SEC A.cpp
@@ -0,0 +1,79 @@
+//                           LAB PRACTICAL 02 ( HOME WORK)                                              
+//                           MUHAMMAD ZAOREZ
+//                           SEC A
+//                           455443
+#include<iostream>
+using namespace std;
+int main()
+{
+	//                                   TASK 03
+/*	int i=0;
+	cout<<"enter year";
+	cin>>i;
+	if(i%4==0){
+		cout<<"year is leap year";
+	}
+	else{
+		cout<<"year is not leap year";
+	}
+	return 0;*/
+//	                                   TASK 02                      
+/*	int i=0;
+	cout<<"enter digit";
+	cin>>i;
+	if(i%2==0&&i%5==0){
+		cout<<"number is both even and divisible by 5";
+	}
+	else{
+		cout<<"this number is not required number";
+	}
+	return 0;*/
+	
+	
+//                       	TASK 01
+	/*int i=0;
+	cout<<"enter marks";
+	cin>>i;
+	if(i>90&&i<=100){
+		cout<<"grade A is awarded";
+	}
+	if(i>75&&i<=90){
+		cout<<"grade B is awarded";
+	}
+	if(i>60&&i<=75){
+		cout<<"grade C is awarded";
+	}
+	if(i>45&&i<=60){
+		cout<<"grade D is awarded";
+	}
+	if(i>0&&i<=45){
+		cout<<"grade E is awarded";
+	}
+		return 0;*/
+		
+		//                          TASK 04
+	/*	float a=0, b=0;
+		cout<<"enter attendance in percentage";
+		cin>>a;
+	    cout<<"enter gpa";
+	    cin>>b;
+	    if(a>=80&&b>=3.5){
+	    	cout<<"eligible for scholarship";
+		}
+	else{
+		cout<<"not eligible for scholarship";
+	}
+	return 0;*/
+	 
+	 //                      TASK 05
+	 /*char character;
+	 cout<<"enter alphabet";
+	 cin>> character;
+	 if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'){
+	 	cout<<"given alphabet is vowel";
+	 }
+	 else{
+	 	cout<<"given alphabet is consonant";
+	 }
+	 return 0;*/
+}
